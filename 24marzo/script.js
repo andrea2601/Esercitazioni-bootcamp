483bcc6d667b9542da476754133011027be5cdaa
andrea2601 <101347536+andrea2601@users.noreply.github.com>
2022-03-24 22:21:05 +0100

    Fix: tutto

--- 24marzo/script.js
+++ 24marzo/script.js
@@ -5,6 +5,9 @@ const poster = document.querySelector("#img");
 const description = document.querySelector("#description");
 const year = document.querySelector("#year");
 const submit = document.querySelector("#submit");
+const hidden = document.querySelector("#hiddenDiv");
+const hiddenDesc = document.querySelector("#hidden");
+
 
 
 
@@ -12,27 +15,65 @@ filmAPI("https://edgemony-backend.herokuapp.com/movies").then(data => {
     data.map(film => {
         createCard(
             film.title,
-            formatMinText(film.description),
+            film.description,
             film.poster,
-            film.year
+            film.year,
+            film.genres,
+            film.id
         );
     })
     const delFilm = (element, key) => {
         element.addEventListener("click", () => {
             console.log("key: ", key);
             cardsWrapper.children[key].classList.add("remove");
-            fetch(`https://edgemony-backend.herokuapp.com/movies/${key + 1}`, {
+            fetch(`https://edgemony-backend.herokuapp.com/movies/${element.id}`, {
                 method: "DELETE",
                 headers: {
                     "Content-Type": "application/json",
                 },
-            });
+            }).then(() => location.reload());
         });
     };
 
     const popUp = (item) => {
         item.addEventListener("click", () => {
-            item.classList.toggle("popUp");
+            const hiddenDesc = document.querySelectorAll(".hiddenDesc");
+            const desc = document.querySelectorAll(".description");
+
+            item.classList.add("popUp");
+            hidden.classList.add("popUp");
+
+            desc.forEach(item => {
+                item.classList.remove("description");
+                item.classList.add("hidden");
+            })
+
+            hiddenDesc.forEach(item => { 
+                item.classList.add("description"); 
+                item.classList.remove("hiddenDesc"); 
+            })
+
+
+            const deleteEls = document.querySelectorAll(".delete");
+            deleteEls.forEach((item) => item.classList.add("hidden"));
+
+            hidden.addEventListener("click", () => {
+
+                item.classList.remove("popUp");
+                hidden.classList.remove("popUp");
+                desc.forEach(item => {
+                    item.classList.add("description");
+                    item.classList.remove("hidden");
+                })
+    
+                hiddenDesc.forEach(item => { 
+                    item.classList.remove("description"); 
+                    item.classList.add("hiddenDesc"); 
+                })
+    
+
+                deleteEls.forEach((item) => item.classList.remove("hidden"));
+            })
         });
     }
 
@@ -46,10 +87,13 @@ filmAPI("https://edgemony-backend.herokuapp.com/movies").then(data => {
 submit.addEventListener("click", (event => {
     event.preventDefault();
 
-    const checkedGeneres = [];
-    const generes = document.querySelectorAll(".generes");
-    generes.forEach(item => {
-        if (item.checked) checkedGeneres.push(item.value)
+    let checkedGenres = [];
+    const genres = document.querySelectorAll(".genres");
+    genres.forEach(item => {
+        if (item.checked == true) {
+            checkedGenres.push(item.value);
+            console.log("....." + checkedGenres);
+        }
     });
 
     fetch("https://edgemony-backend.herokuapp.com/movies", {
@@ -62,7 +106,7 @@ submit.addEventListener("click", (event => {
             description: description.value,
             poster: poster.value,
             year: year.value,
-            generes: checkedGeneres
+            genres: checkedGenres
         })
     }).then((data) => location.reload());
 }))
--- 24marzo/utils.js
+++ 24marzo/utils.js
@@ -1,27 +1,34 @@
 const cardsWrapper = document.querySelector(".cardsWrapper");
-const createCard = (title, desc, imgUrl, year) => {
+const createCard = (title, desc, imgUrl, year, genres, id) => {
     const divEl = document.createElement("div");
     const h3El = document.createElement("h3");
     const imgEl = document.createElement("img");
     const parDescEl = document.createElement("p");
+    const parDescElExt = document.createElement("p");
     const parYearEl = document.createElement("p");
+    const parGenEl = document.createElement("p");
     const x = document.createElement("p");
 
 
     divEl.classList.add("card");
     parYearEl.classList.add("year");
     parDescEl.classList.add("description");
+    parDescElExt.classList.add("hiddenDesc");
+    parGenEl.classList.add("genres");
     x.classList.add("delete");
+    x.setAttribute("id", id);
 
     imgEl.setAttribute("src", imgUrl);
     imgEl.setAttribute("alt", "film cover");
 
     h3El.textContent = title;
-    parDescEl.textContent = desc;
-    parYearEl.textContent = year;
+    parDescEl.textContent = formatMinText(desc);
+    parDescElExt.textContent = desc;
+    parYearEl.textContent = `Anno: ${year}`;
+    parGenEl.textContent = `Generi: ${genres}`;
     x.textContent = "x";
 
-    divEl.append(h3El, x, imgEl, parDescEl, parYearEl);
+    divEl.append(h3El, x, imgEl, parDescEl, parDescElExt, parYearEl, parGenEl);
     cardsWrapper.appendChild(divEl);
 };
 
@@ -35,6 +42,6 @@ const filmAPI = async (url) => {
     return await res.json();    
 };
 
-const formatMinText = (text) => text.split(" ").slice(0, 6).join(" ") + " ...";
+const formatMinText = (text) => text.split("").slice(0, 25).join("") + " ...";
 
 export {createCard, filmAPI, formatMinText};
